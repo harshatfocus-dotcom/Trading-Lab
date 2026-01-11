83e88e9fab7b442e534060725fe23a68d9b2cb2b
harshatfocus-dotcom <harshatfocus@gmail.com>
2026-01-11 22:56:17 +0530

    Delete trading-lab directory

--- trading-lab/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ExperimentPlatform from "./ExperimentPlatform";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ExperimentPlatform />
-  </React.StrictMode>
-);
